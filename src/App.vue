cda4c628c6dcbc43fa1c58b96f810766d5c9e5c1
Elia Magris <elia.magris@libero.it>
2022-11-25 16:11:00 +0100

    esercizio completato

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <div class="logo">
+      <img src="../src/assets/img/logo-small.svg" alt="" />
+    </div>
+    <div class="mainContainer">
+      <div class="cardContainer">
+        <CardsItem
+          v-for="(element, index) in infoCard"
+          :key="index"
+          :card="element"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardsItem from "./components/CardsItem.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    CardsItem,
+  },
+
+  data() {
+    return {
+      infoCard: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        this.infoCard = response.data.response;
+        console.log(this.infoCard);
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: rgb(30, 45, 59);
+
+  .logo {
+    padding: 15px;
+    text-align: start;
+    background-color: rgb(46, 58, 70);
+    img {
+      width: 3%;
+    }
+  }
+
+  .mainContainer {
+    display: flex;
+    justify-content: center;
+  }
+
+  .cardContainer {
+    display: flex;
+    flex-wrap: wrap;
+    width: 80%;
+    padding: 20px;
+  }
+}
+</style>
---
+++ src/components/CardsItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="card" style="width: 18rem">
+      <img :src="card.poster" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{{ card.title }}</h5>
+
+        <div class="card-text">
+          <div>{{ card.author }}</div>
+          <div>{{ card.year }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardsItem",
+  props: {
+    card: Object
+  },
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container{
+  width: calc(100% / 5);
+  padding: 35px 25px;
+
+  .card{
+    width: 100% !important;;
+    height: 350px;
+    margin: 5px;
+    background-color: rgb(46,58,70);
+
+    img{
+      padding: 10px;
+    }
+
+    .card-title{
+      color: white;
+      font-weight: bold;
+    }
+
+    .card-text{
+      color: gray;
+    }
+  }
+}
+</style>
