bcdee287cb4b11bc5aa99515d75528dce3186663
EliaMagris <elia.magris@libero.it>
2022-11-28 16:59:41 +0100

    esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <div class="logo">
-      <img src="../src/assets/img/logo-small.svg" alt="" />
-    </div>
+    <headerBig/>
     <div>
       <select name="" id="" v-model="selectGen">
+        <option value="" selected>SEE ALL</option>
         <option :value="elem" v-for="(elem, index) in infoSelect" :key="index">{{ elem }}</option>
       </select>
     </div>
     <div class="mainContainer">
       <div class="cardContainer">
         <CardsItem
-          v-for="(element, index) in infoCard"
+          v-for="(element, index) in funzioneControllo"
           :key="index"
           :card="element"
-          :class="element.genre == selectGen ? 'd-block' : 'd-none'"
         />
       </div>
     </div>
@@ -23,24 +21,44 @@
 
 <script>
 import CardsItem from './components/CardsItem.vue';
+import headerBig from './components/headerBig.vue';
 import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     CardsItem,
+    headerBig
   },
 
   data() {
     return {
       infoCard: [],
       infoSelect: [],
-      selectGen: '',
+      selectGen: [],
     };
   },
 
+  computed: {
+    funzioneControllo(){
+      if (this.selectGen == ""){
+        return this.infoCard
+      }else{
+        return this.infoCard.filter((elem) =>{
+          return elem.genre == this.selectGen
+        })
+      }
+    }
+
+  },
+
   mounted() {
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
+    this.filterImage()
+  },
+
+  methods: {
+    filterImage(){
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
       this.infoCard = response.data.response;
       console.log(this.infoCard);
       this.infoCard.forEach((SingleGen) => {
@@ -49,7 +67,8 @@ export default {
         }
       });
     });
-  },
+    }
+  }
 };
 </script>
 
---
+++ src/components/headerBig.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="logo">
+      <img src="../assets/img/logo-small.svg" alt="" />
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'headerBig',
+
+};
+</script>
+
+<style lang="scss">
+  .logo {
+    padding: 15px;
+    text-align: start;
+    background-color: rgb(46, 58, 70);
+    img {
+      width: 3%;
+    }
+  }
+
+</style>
